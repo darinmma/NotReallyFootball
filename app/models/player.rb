1920e648f147e656dd0c4e38935c2f2ce3c3369a
Darin Ma <darinmma@gmail.com>
2016-11-01 18:58:12 -0700

    fixed my code hopefully

--- app/models/player.rb
+++
@@ -1,4 +0,0 @@
-class Player < ApplicationRecord
-  belongs_to :team
-  belongs_to :pos
-end
--- db/schema.rb
+++ db/schema.rb
@@ -15,12 +15,6 @@ ActiveRecord::Schema.define(version: 20161102004206) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
-  create_table "players", force: :cascade do |t|
-    t.string   "name"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
-
   create_table "positions", force: :cascade do |t|
     t.string   "pos"
     t.datetime "created_at", null: false
