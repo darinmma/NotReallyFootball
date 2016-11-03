cf4c28cbc57a03866f0da7c7b8e86aa68d9d0065
Darin Ma <darinmma@gmail.com>
2016-11-03 08:59:05 -0700

    fixes login by adding root post route, update index navigation links

--- config/routes.rb
+++ config/routes.rb
@@ -17,5 +17,6 @@ Rails.application.routes.draw do
 
   get 'login/' => 'sessions#new'
   post 'login/' => 'sessions#create'
+  post '/' => 'sessions#create'
   delete 'logout/' => 'sessions#destroy'
 end
